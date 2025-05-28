82f7dc0cd09c666d7d8da84bbfce41e4651ffae7
Brandon Helms <brandon.helms@dbtlabs.com>
2025-05-28 13:45:44 -0500

    converting to typescript

--- src/js/config/env.js
+++
@@ -1,9 +0,0 @@
-// Environment configuration
-const env = {
-    // In production, these values will be replaced during build time
-    // For development, we use default values
-    OKTA_DOMAIN: 'trial-5939542.okta.com',
-    OKTA_CLIENT_ID: '0oartsuih3QYwbQws697'
-};
-
-export default env; 
